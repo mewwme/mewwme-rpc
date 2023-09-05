f845991677d35985ccac84d4abcc06c009bed9b5
lrmn7 <lrmn.dev@gmail.com>
2023-09-05 20:06:37 +0700

    promote create

--- index.js
+++ index.js
@@ -24,24 +24,24 @@ client.on('ready', async () => {
 
   const r = new Discord.RichPresence()
     .setApplicationId('1091763429779443844')
-    .setType('LISTENING')
+    .setType('PLAYING')
     .setURL('https://www.youtube.com/watch?v=xvFZjo5PgG0')
-    .setState('Youtube, Spotify & Soundcloud, Etc')
-    .setName('Moo ♪')
-    .setDetails(`Moo ♪ a discord bot music`)
+    .setState('Moderation, Music, Fun, Utility, Etc')
+    .setName('Lucy ♡')
+    .setDetails(`a discord moderation & music  24/7`)
     .setStartTimestamp(Date.now())
-.setAssetsLargeImage('https://cdn.discordapp.com/attachments/1098969636306960465/1145731562021859339/moo.png')
+.setAssetsLargeImage('https://cdn.discordapp.com/attachments/1098969636306960465/1148603329291763834/luciaaa.png')
     .setAssetsLargeText('Moo ♪')
     .setAssetsSmallImage('https://cdn.discordapp.com/emojis/917227945712562207.gif?size=96&quality=lossless')
     .setAssetsSmallText('Verified')
-    .addButton('Invite Moo ♪', 'https://discord.com/api/oauth2/authorize?client_id=1135151608565616662&permissions=689379470656&scope=applications.commands%20bot')
-    .addButton('Website Moo ♪', 'https://moo.is-a.fun/');
+    .addButton('Invite Lucy ♡', 'https://discord.com/api/oauth2/authorize?client_id=928966154817523723&permissions=1073080662&scope=applications.commands%20bot&redirect_uri=https%3A%2F%2Flucy.hop.sh%2Fapi%2Fcallback')
+    .addButton('Website Lucy ♡', 'https://lucy.is-a.fun/');
   client.user.setActivity(r);
   client.user.setPresence({ status: "idle" });
 
   setInterval(() => {
     const newTime = formatTime();
-    const newDetails = `Moo ♪ a discord bot music`; //`are you sleeping?`;
+    const newDetails = `a discord moderation & music  24/7`; //`are you sleeping?`;
     r.setDetails(newDetails);
     client.user.setActivity(r);
   }, 1000); // Update every second
